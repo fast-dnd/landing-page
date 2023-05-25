a59a6848acb7bcce767b6e38a2dad92c19b24e74
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-05-25 16:38:30 +0200

    create project layout

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Basics from "@/components/Basics";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import Welcome from "@/components/Welcome";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Image src="/assets/bg-welcome.png" alt="hero" fill className="-z-10" />
+        <Header />
+        <Welcome />
+      </div>
+      <Intro />
+      <Basics />
+      <Footer />
+    </main>
+  );
+}
