79727244570d0807b3614b4ce4bc1298ed699d10
vita-ride <viktornovakovic77@gmail.com>
2023-05-27 00:56:33 +0200

    Page style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full">
-      <div>
-        <Image src="/assets/bg-welcome.png" alt="hero" fill className="-z-10" />
-        <Header />
-        <Welcome />
+      <div className="w-full relative">
+        <Image src="/assets/bg-welcome.png" alt="hero" width={1920} height={1080} className="-z-10 w-full h-[1080px]" />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col">
+          <Header />
+          <Welcome />
+        </div>
       </div>
       <Intro />
       <Basics />
