4e84862a6b4dd495df28724e70d75160a9f660b0
vita-ride <viktornovakovic77@gmail.com>
2023-05-29 16:32:18 +0200

    Fix red layer opacity

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -9,15 +9,15 @@ const Welcome = () => {
     <div className="mt-40 flex items-center justify-center flex-col gap-3">
       <Image src="/images/logo-up.png" width={266} height={15} alt="logo-up" />
       <div className="relative w-[475.22px] flex justify-center">
-        <div className="absolute w-full h-full flex items-center justify-center -z-10">
+        <div className="absolute w-full h-full flex items-center justify-center">
           <Image className="" src="/images/logo-red-layer.png" width={475.22} height={81.86} alt="logo-red-layer" />
         </div>
-        <div className="absolute w-full h-full flex items-center justify-center z-10">
+        <div className="absolute w-full h-full flex items-center justify-center z-20">
           <p style={jibril.style} className="text-6xl tracking-[0.415em] text-center indent-[0.415em] uppercase">
             v3rpg
           </p>
         </div>
-        <Image className="" src="/images/logo-black-layer.png" alt="logo-black-layer" width={463.51} height={97.05} />
+        <Image className="z-10" src="/images/logo-black-layer.png" alt="logo-black-layer" width={463.51} height={97.05} />
       </div>
       <Image className="mb-4" src="/images/logo-down.png" width={167} height={32} alt="logo-down" />
       <div className="text-[24px] tracking-[0.2em] leading-[46px] uppercase">
