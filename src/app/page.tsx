da49f7721b5be10f9fc28dbdb1f7e772d0ad0881
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-06-24 23:18:03 +0200

    lingrad layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="w-[1600px] mx-auto lingrad">
       <div className="w-full relative">
         <Image src="/images/bg-welcome.png" alt="hero" width={1920} height={1080} className="-z-10 w-full h-[1080px]" />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col">
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col lingrad">
           <Header />
           <Welcome />
         </div>
