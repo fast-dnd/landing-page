0a5b693ead60b554afe05d2035711fcb1db92c85
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-10-11 21:26:11 +0200

    seo opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,24 @@ import "./globals.css";
 import { lexend } from "@/utils/fonts";
 
 export const metadata: Metadata = {
-  title: "V3RPG",
-  description: "AI powered RPG",
+  metadataBase: new URL("https://landing.v3rpg.com/"),
+  title: "v3RPG",
+  description: "Bringing gamification to storytelling. Powered by AI.",
   icons: {
     icon: "/v3dnd-logo.svg",
   },
+  openGraph: {
+    images: ["/v3dnd-logo.png"],
+    title: "v3RPG",
+    description: "Bringing gamification to storytelling. Powered by AI.",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={lexend.className}>{children}</body>
